42ae889bc59a199f52d08cfd246862f7cd48823b
ipeavy <ipeavy401@gmail.com>
2022-04-01 17:34:05 -0400

    added html files

---
+++ gulpfile.js/tasks/html.js
@@ -0,0 +1,9 @@
+const html = () => {
+	return $.gulp.src($.path.html.src)
+	.pipe($.plugins.fileInclude())
+	.pipe($.plugins.htmlmin($.settings.htmlmin))
+	.pipe($.gulp.dest($.path.html.dest))
+	.pipe($.browserSync.stream());
+}
+
+module.exports = html;
